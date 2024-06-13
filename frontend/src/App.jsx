d9d0fb4f63be9b78633d896108cf16226f7d4c90
Akshay More <AkshayM@proteantech.in>
2024-06-13 17:20:48 +0530

    frontend and backend connected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+    .then((response) => {
+      setJokes(response.data)
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  })
 
   return (
     <>
       <h1>Welcome to Home Page</h1>
+
+      <p>JOKES: {jokes.length}</p>
+      {jokes.map((joke, index) => (
+        <div key = {joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      ))}
     </>
   )
 }
--- index.js
+++ index.js
@@ -13,6 +13,33 @@ app.get("/", (req, res) => {
   res.send("hellow from send method");
 });
 
+app.get("/api/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: 'first joke',
+      content: 'this is first joke'
+    },
+    {
+      id: 2,
+      title: 'second joke',
+      content: 'this is second joke'
+    },
+    {
+      id: 3,
+      title: 'third joke',
+      content: 'this is third joke'
+    },
+    {
+      id: 4,
+      title: 'fourth joke',
+      content: 'this is fourth joke'
+    }
+  ]
+
+  res.send(jokes)
+})
+
 app.get("/login", (req, res) => {
   res.send("welcome to login page");
 });
